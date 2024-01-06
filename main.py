1d56d81599905bbb599c6755d33532aefe2c0aa9
Sydel Palinlin <sydel.palinlin@gmail.com>
2024-01-07 07:40:50 +0800

    Remove unused imports

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from app.core import Core
 from app.services.mosquitto import Mosquitto
 from environment import (
-    APP_ENVIRONMENT,
     MQTT_BROKER,
     MQTT_CLIENT_ID,
-    MQTT_PASSWORD,
     MQTT_PORT,
     MQTT_TOPIC_PUB,
     MQTT_TOPIC_SUB,
-    MQTT_USERNAME,
 )
-
-# Ensure that MQTT_USERNAME and MQTT_PASSWORD are used appropriately in Mosquitto class
 
 mosquitto = Mosquitto(
     MQTT_CLIENT_ID, MQTT_BROKER, MQTT_PORT, MQTT_TOPIC_SUB, MQTT_TOPIC_PUB
